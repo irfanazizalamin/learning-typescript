5cecedb2ee88617d125a401ad98a0989a78fc0e1
Irfan Aziz <irfan.aziz@rumahsiapkerja.com>
2022-03-24 11:38:34 +0700

    [dataType] number-boolean

--- build/dataType.js
+++ build/dataType.js
@@ -1,3 +1,10 @@
 "use strict";
+// string
 let firstName = "Irfan";
 firstName = '';
+// number
+let age = 102;
+age = 103;
+// boolean
+let isSingle = true;
+isSingle = false;
--- src/dataType.ts
+++ src/dataType.ts
@@ -1,3 +1,11 @@
+// string
 let firstName = "Irfan"
-
 firstName = ''
+
+// number
+let age: number = 102
+age = 103
+
+// boolean
+let isSingle: boolean = true
+isSingle = false

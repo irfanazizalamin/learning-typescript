229abcb8dd6582683205ab25f0a7f250144c94d9
Irfan Aziz <irfan.aziz@rumahsiapkerja.com>
2022-03-25 08:37:17 +0700

    [dataType] enums

---
+++ build/enum.js
@@ -0,0 +1,20 @@
+"use strict";
+// enum
+// // numeric enums
+// enum Month {
+//   JAN = 1,
+//   FEB,
+//   MAR,
+//   APR,
+//   MAY
+// }
+// string enums
+var Month;
+(function (Month) {
+    Month["JAN"] = "Januari";
+    Month["FEB"] = "Februari";
+    Month["MAR"] = "Maret";
+    Month["APR"] = "April";
+    Month["MAY"] = "Mei";
+})(Month || (Month = {}));
+console.log('Month', Month.JAN);
---
+++ src/enum.ts
@@ -0,0 +1,21 @@
+// enum
+
+// // numeric enums
+// enum Month {
+//   JAN = 1,
+//   FEB,
+//   MAR,
+//   APR,
+//   MAY
+// }
+
+
+// string enums
+enum Month {
+  JAN = 'Januari',
+  FEB = 'Februari',
+  MAR = 'Maret',
+  APR = 'April',
+  MAY = 'Mei'
+}
+console.log('Month', Month.JAN)

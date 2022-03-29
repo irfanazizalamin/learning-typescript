cb02e4ab52950b0fa2d784fd4b5d749b74452147
Irfan Aziz <irfan.aziz@rumahsiapkerja.com>
2022-03-29 16:36:54 +0700

    function - with params

--- build/function.js
+++ build/function.js
@@ -10,4 +10,17 @@ const handleCallNameAndAge = () => {
     // return ''  // we cant return anything because we declare void to this method
     console.log(getName(), getAge());
 };
-handleCallNameAndAge();
+// handleCallNameAndAge()
+// function with argument
+const multiply = (val1, val2) => {
+    return val1 * val2;
+};
+const add = ({ val1, val2 }) => {
+    return val1 + val2;
+};
+// we should define parameters as any instead of nothing
+const subtract = (val1, val2) => {
+    return val1 - val2;
+};
+const result = multiply(2, 4);
+console.log('result', result);
--- src/function.ts
+++ src/function.ts
@@ -13,4 +13,24 @@ const handleCallNameAndAge = (): void => {
   console.log(getName(), getAge())
 }
 
-handleCallNameAndAge()
+// handleCallNameAndAge()
+
+
+
+// function with argument
+
+const multiply = (val1: number, val2: number) => {
+  return val1 * val2
+}
+
+const add = ({ val1, val2 }: { val1:number, val2: number }) => {
+  return val1 + val2
+}
+
+// we should define parameters as any instead of nothing
+const subtract = (val1: any, val2: any) => {
+  return val1 - val2
+}
+
+const result = multiply(2, 4)
+console.log('result', result)
